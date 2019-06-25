c3861c6a24516e33385b1b73216dc7fe5dbaf16b
Gilberto Castillo <gilberto.castillo@rogers.com>
2019-06-24 21:29:43 -0400

    Update main.tf

--- infrastructure-as-code/k8s-vault-config/main.tf
+++ infrastructure-as-code/k8s-vault-config/main.tf
@@ -10,7 +10,7 @@ resource "vault_auth_backend" "k8s" {
 }
 
 provider "vault" {
-  address = "${data.terraform_remote_state.k8s_cluster.vault_addr}"
+  address = "${data.terraform_remote_state.outputs.vault_addr}"
 }
 
 data "terraform_remote_state" "k8s_cluster" {
@@ -21,10 +21,10 @@ data "terraform_remote_state" "k8s_cluster" {
 }
 
 provider "kubernetes" {
-  host = "${data.terraform_remote_state.k8s_cluster.k8s_endpoint}"
-  client_certificate = "${base64decode(data.terraform_remote_state.k8s_cluster.k8s_master_auth_client_certificate)}"
-  client_key = "${base64decode(data.terraform_remote_state.k8s_cluster.k8s_master_auth_client_key)}"
-  cluster_ca_certificate = "${base64decode(data.terraform_remote_state.k8s_cluster.k8s_master_auth_cluster_ca_certificate)}"
+  host = "${data.terraform_remote_state.outputs.k8s_endpoint}"
+  client_certificate = "${base64decode(data.terraform_remote_state.outputs.k8s_master_auth_client_certificate)}"
+  client_key = "${base64decode(data.terraform_remote_state.outputs.k8s_master_auth_client_key)}"
+  cluster_ca_certificate = "${base64decode(data.terraform_remote_state.outputs.k8s_master_auth_cluster_ca_certificate)}"
 }
 
 resource "kubernetes_service_account" "vault_reviewer" {
@@ -56,8 +56,8 @@ data "null_data_source" "read_token" {
 # instead of the a curl command in local-exec
 resource "vault_kubernetes_auth_backend_config" "auth_config" {
   backend = "${vault_auth_backend.k8s.path}"
-  kubernetes_host = "https://${data.terraform_remote_state.k8s_cluster.k8s_endpoint}:443"
-  kubernetes_ca_cert = "${chomp(base64decode(data.terraform_remote_state.k8s_cluster.k8s_master_auth_cluster_ca_certificate))}"
+  kubernetes_host = "https://${data.terraform_remote_state.outputs.k8s_endpoint}:443"
+  kubernetes_ca_cert = "${chomp(base64decode(data.terraform_remote_state.outputs.k8s_master_auth_cluster_ca_certificate))}"
   token_reviewer_jwt = "${data.null_data_source.read_token.outputs["token"]}"
 }
 
@@ -68,6 +68,6 @@ resource "vault_kubernetes_auth_backend_role" "role" {
   role_name = "demo"
   bound_service_account_names = ["cats-and-dogs"]
   bound_service_account_namespaces = ["default", "cats-and-dogs"]
-  policies = ["${data.terraform_remote_state.k8s_cluster.vault_user}"]
+  policies = ["${data.terraform_remote_state.outputs.vault_user}"]
   ttl = 7200
 }
